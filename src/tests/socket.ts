84e0898833c747df088a8dac6ff28e9b18aa5989
Payton <paytonmk744@outlook.com>
2021-10-03 16:57:41 -0500

    Added socket test

--- src/tests/socket.ts
+++ src/tests/socket.ts
@@ -1,5 +1,25 @@
-import { socket } from '../beapi/BeAPI.js'
+import {
+  socket,
+  commands,
+  world,
+} from '../beapi/BeAPI.js'
+
+commands.registerCommand({
+  command: "sk-t",
+  description: "Send a ping to BeRP SocketManager",
+}, (data) => {
+  if (!socket.enabled) return data.sender.sendMessage('Socket is disabled! Please connect a BeRP bot to use sockets.')
+  socket.sendMessage({
+    berp: {
+      event: "Ping",
+      requestId: "",
+    },
+  })
+
+  return data.sender.sendMessage("Ping sent! Awaiting response!")
+})
 
 socket.on("Message", (data) => {
-  data
+  if (data.event != "Pong") return
+  world.sendMessage('Pong! Sent from BeRP.')
 })
