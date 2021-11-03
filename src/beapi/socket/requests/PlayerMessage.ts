825080b3012fb686e24bbf5aad20659fb6750208
PMK744 <69542280+PMK744@users.noreply.github.com>
2021-11-02 21:14:11 -0500

    Updated PlayerMessage Request

--- src/beapi/socket/requests/PlayerMessage.ts
+++ src/beapi/socket/requests/PlayerMessage.ts
@@ -1,6 +1,7 @@
 import { SocketManager } from '../SocketManager.js'
 import { events } from '../../events/EventManager.js'
 import { newRequestId } from '../requestId.js'
+import { World } from 'mojang-minecraft'
 
 export class PlayerMessage {
   private _socket: SocketManager
@@ -9,14 +10,14 @@ export class PlayerMessage {
 
   constructor(socket: SocketManager) {
     this._socket = socket
-    events.on('PlayerMessage', (data) => {
+    World.events.beforeChat.subscribe((data) => {
       this._socket.sendMessage({
         berp: {
           event: "PlayerMessage",
-          sender: data.sender.getName() || data.sender.getNameTag(),
+          sender: data.sender.name || data.sender.nameTag,
           player: {
-            name: data.sender.getName(),
-            nameTag: data.sender.getNameTag().replace(/\n/g, "*n"),
+            name: data.sender.name,
+            nameTag: data.sender.nameTag.replace(/\n/g, "*n"),
           },
           message: data.message.replace(/\\/g, "/"),
           requestId: `${newRequestId()}`,
