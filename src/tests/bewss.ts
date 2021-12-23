52770897b8fcf42e288f7448fbbf4be39349a5ed
Payton <paytonmk744@outlook.com>
2021-12-23 15:36:19 -0700

    Updated bewss test script

--- src/tests/bewss.ts
+++ src/tests/bewss.ts
@@ -1,7 +1,11 @@
 import {
   events,
+  commands,
 } from '../beapi/BeAPI.js'
 
+// Import this script when using BeWss. This will emit all messages.
+
+commands.enabled = false
 events.on("PlayerMessage", (data) => {
   const message = data.message.split(' ').join('|')
   data.sender.addTag(`bewss:msg:${message}`)
