bd65d5232b25829128df8e8c27af242f52e4b752
Payton <paytonmk744@outlook.com>
2022-01-05 15:25:12 -0600

    fix(version): updated protocol

--- packages/beapi/src/version.ts
+++ packages/beapi/src/version.ts
@@ -2,7 +2,7 @@
 import { version as v } from '../package.json'
 const version = v
 const mcbe = '1.18.10'
-const protocol = 479
+const protocol = 485
 const build = 'dev'
 
 export { version, mcbe, protocol, build }
