b9854684546da845432b23f1d1afee5510588826
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2022-01-07 20:07:16 -0500

    Added a executeCommands
    so players can execute multiple commands in the same function

--- packages/beapi/src/command/executeCommand.ts
+++ packages/beapi/src/command/executeCommand.ts
@@ -1,31 +1,69 @@
-import { world } from 'mojang-minecraft'
-import type { ExecuteCommandResponse, Dimensions } from '../@types/BeAPI.i'
+import { world } from "mojang-minecraft";
+import type { ExecuteCommandResponse, Dimensions } from "../@types/BeAPI.i";
 
-function executeCommand(cmd: string, dimension?: Dimensions, debug = false): ExecuteCommandResponse {
-  const command = cmd.replace(/\\/g, '')
+function executeCommand(
+  cmd: string,
+  dimension?: Dimensions,
+  debug = false
+): ExecuteCommandResponse {
+  const command = cmd.replace(/\\/g, "");
 
   try {
-    if (!dimension) dimension = 'overworld'
-    const cmd = world.getDimension(dimension).runCommand(command)
+    if (!dimension) dimension = "overworld";
+    const cmd = world.getDimension(dimension).runCommand(command);
 
     return {
       statusMessage: cmd.statusMessage,
       data: cmd,
       err: false,
-    }
+    };
   } catch (err) {
     if (!debug)
       return {
         statusMessage: `Error Occured: ${err}`,
         err: true,
-      }
-    console.warn(`[BeAPI] [executeCommand]${err}`)
+      };
+    console.warn(`[BeAPI] [executeCommand]${err}`);
 
     return {
-      statusMessage: 'Error Occured',
+      statusMessage: "Error Occured",
       err: true,
-    }
+    };
   }
 }
 
-export { executeCommand }
+function executeCommands(
+  cmds: Array<string>,
+  dimension?: Dimensions,
+  debug = false
+): ExecuteCommandResponse {
+
+  try {
+    if (!dimension) dimension = "overworld";
+    let data = [];
+    cmds.forEach((cmd) => {
+      data.push(
+        world.getDimension(dimension).runCommand(cmd.replace(/\\/g, ""))
+      );
+    });
+
+    return {
+      data: data,
+      err: false,
+    };
+  } catch (err) {
+    if (!debug)
+      return {
+        statusMessage: `Error Occured: ${err}`,
+        err: true,
+      };
+    console.warn(`[BeAPI] [executeCommand]${err}`);
+
+    return {
+      statusMessage: "Error Occured",
+      err: true,
+    };
+  }
+}
+
+export { executeCommand, executeCommands };
