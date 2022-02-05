f86e5e7f3fac42503a837fe8d47911ed29c9623c
PMK744 <69542280+PMK744@users.noreply.github.com>
2022-02-05 09:45:17 -0600

    fix: updated version and protocol

--- packages/beapi/src/version.ts
+++ packages/beapi/src/version.ts
@@ -1,5 +1,5 @@
 // @ts-ignore No Other Way :)
 import { version as v } from '../package.json'
 export const version = v
-export const mcbe = '1.18.10'
-export const protocol = 485
+export const mcbe = '1.18.20'
+export const protocol = 490
