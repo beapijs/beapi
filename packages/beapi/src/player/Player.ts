e351657f63f783ea8de7adfd279953dc78c73bd0
PMK744 <69542280+PMK744@users.noreply.github.com>
2022-02-12 00:41:44 -0600

    fix(player): fixed getName

--- packages/beapi/src/player/Player.ts
+++ packages/beapi/src/player/Player.ts
@@ -14,11 +14,13 @@ import type { Location, Dimension, Gamemode, ServerCommandResponse, PlayerCompon
 export class Player {
   protected readonly _client: Client
   protected readonly _IPlayer: IPlayer
+  protected readonly _name: string
   public prevPlayerInVector: Player | undefined
   public prevEntityInVector: Entity | undefined
   public constructor(client: Client, player: IPlayer) {
     this._client = client
     this._IPlayer = player
+    this._name = player.name
   }
 
   public destroy(reason = 'Instantiated player object destroyed!'): void {
@@ -30,7 +32,7 @@ export class Player {
   }
 
   public getName(): string {
-    return this._IPlayer.name
+    return this._name
   }
 
   public getNameTag(): string {
