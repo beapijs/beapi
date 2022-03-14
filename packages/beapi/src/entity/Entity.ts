a8d5ab59402adf25a6704787adc1c7654f0fbdc5
Payton <paytonmk744@outlook.com>
2022-03-14 12:08:48 -0600

    fix(entity): updated entity to use Objective

--- packages/beapi/src/entity/Entity.ts
+++ packages/beapi/src/entity/Entity.ts
@@ -8,7 +8,7 @@ import type {
   Vector,
 } from 'mojang-minecraft'
 import type { Client } from '../client'
-import type { Location, Dimension, ServerCommandResponse, EntityComponents } from '../types'
+import type { Location, Dimension, ServerCommandResponse, EntityComponents, Objective } from '../types'
 import { Location as ILocation } from 'mojang-minecraft'
 import { getUniqueId } from '../utils'
 
@@ -94,27 +94,27 @@ export class Entity {
     }
   }
 
-  public getScore(objective: string): number {
-    const command = this.executeCommand(`scoreboard players test @s "${objective}" * *`)
+  public getScore(objective: Objective): number {
+    const command = this.executeCommand(`scoreboard players test @s "${objective.id}" * *`)
     if (command.err) return 0
 
     return parseInt(String(command.statusMessage?.split(' ')[1]), 10)
   }
 
-  public setScore(objective: string, amount: number): number {
-    this.executeCommand(`scoreboard players set @s "${objective}" ${amount}`)
+  public setScore(objective: Objective, amount: number): number {
+    this.executeCommand(`scoreboard players set @s "${objective.id}" ${amount}`)
 
     return this.getScore(objective)
   }
 
-  public addScore(objective: string, amount: number): number {
-    this.executeCommand(`scoreboard players add @s "${objective}" ${amount}`)
+  public addScore(objective: Objective, amount: number): number {
+    this.executeCommand(`scoreboard players add @s "${objective.id}" ${amount}`)
 
     return this.getScore(objective)
   }
 
-  public removeScore(objective: string, amount: number): number {
-    this.executeCommand(`scoreboard players remove @s "${objective}" ${amount}`)
+  public removeScore(objective: Objective, amount: number): number {
+    this.executeCommand(`scoreboard players remove @s "${objective.id}" ${amount}`)
 
     return this.getScore(objective)
   }
