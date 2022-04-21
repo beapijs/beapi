3be60ac57e545ee38f585a868da8fcc9dca32c44
Nobu <chat@nobu.sh>
2022-04-21 01:09:01 -0500

    refactor(imports): reduce amount of imports used

--- packages/beapi/src/events/Respawn.ts
+++ packages/beapi/src/events/Respawn.ts
@@ -1,10 +1,5 @@
 // Regular imports.
-import AbstractEvent from './AbstractEvent'
-import { setProto } from '../'
-
-// Type imports.
-import type { PlayerTagEvent } from '..'
-import type { Client } from '../client'
+import { setProto, AbstractEvent, PlayerTagEvent, Client } from '..'
 
 /**
  * BeAPI respawn event. Contains the logic
