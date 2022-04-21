8d025357ae81732c7fc2fa79cc9b163e7c323f9f
Nobu <chat@nobu.sh>
2022-04-21 01:06:07 -0500

    refactor(imports): reduce amount of imports used

--- packages/beapi/src/events/BlockCreated.ts
+++ packages/beapi/src/events/BlockCreated.ts
@@ -1,11 +1,6 @@
 // Regular imports.
-import AbstractEvent from './AbstractEvent'
+import { AbstractEvent, setProto, Block, Client } from '..'
 import { world, BlockPlaceEvent } from 'mojang-minecraft'
-import { Block } from '../block'
-import { setProto } from '../'
-
-// Type imports.
-import type { Client } from '../client'
 
 /**
  * BeAPI block created event. Contains the logic
