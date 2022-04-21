7e294c957151b6169b0b4c9e3b81cafc97ca911f
Nobu <chat@nobu.sh>
2022-04-21 01:07:08 -0500

    refactor(imports): reduce amount of imports used

--- packages/beapi/src/events/Explosion.ts
+++ packages/beapi/src/events/Explosion.ts
@@ -1,10 +1,6 @@
 // Regular imports.
-import AbstractEvent from './AbstractEvent'
-import { setProto } from '../'
+import { setProto, AbstractEvent, Client } from '..'
 import { BeforeExplosionEvent, world } from 'mojang-minecraft'
-
-// Type imports.
-import type { Client } from '../client'
 
 /**
  * BeAPI explosion event. Contains the logic
