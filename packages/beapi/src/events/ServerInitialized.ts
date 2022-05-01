3f874bdbfc682c32103359dbe96f300bcb8b0baf
PMK744 <69542280+PMK744@users.noreply.github.com>
2022-05-01 14:37:54 -0500

    fix(): added EntityType to ServerInitilized event

--- packages/beapi/src/events/ServerInitialized.ts
+++ packages/beapi/src/events/ServerInitialized.ts
@@ -1,11 +1,6 @@
 import * as Minecraft from 'mojang-minecraft'
-import { world } from 'mojang-minecraft'
-import { setProto, Client, PropertyRegistry, PropertyType, DynamicPropertiesDefinition, AbstractEvent } from '..'
-
-// FIXME: TEMP - Mojang keep on top of your typings or hire me at <Nobu : chat@nobu.sh>
-export interface WorldInitializeEvent {
-  propertyRegistry: PropertyRegistry
-}
+import { world, WorldInitializeEvent, EntityType } from 'mojang-minecraft'
+import { setProto, Client, PropertyType, DynamicPropertiesDefinition, AbstractEvent } from '..'
 
 /**
  * BeAPI ServerInitialized event. Contains the logic
@@ -48,7 +43,6 @@ export class ServerInitialized extends AbstractEvent {
     if (!this._registered) {
       // Subscribe to Minecraft world event with IName
       // And use bound _logic for the callback.
-      // @ts-ignore FIXME: TEMP - util Minecraft typings are updated.
       world.events[this.iName].subscribe(this._logic)
       // Set registered to true so this cannot be called
       // Again before off being called.
@@ -62,7 +56,6 @@ export class ServerInitialized extends AbstractEvent {
     if (this._registered) {
       // Remove Minecraft event listener on IName
       // With bound _logic callback.
-      // @ts-ignore FIXME: TEMP - util Minecraft typings are updated.
       world.events[this.iName].unsubscribe(this._logic)
       // Set registered to false so this cannot be called
       // Again before on being called.
@@ -90,8 +83,7 @@ export class ServerInitialized extends AbstractEvent {
             return arg.propertyRegistry.registerWorldDynamicProperties(dynamic)
         }
       },
-      // TODO: Fix typings once added by mojang
-      registerEntityProperty: (entity: any, property: PropertyType, id: string, length = 10): void => {
+      registerEntityProperty: (entity: EntityType, property: PropertyType, id: string, length = 10): void => {
         const dynamic = new Minecraft.DynamicPropertiesDefinition() as DynamicPropertiesDefinition
         switch (property) {
           case 'string':
