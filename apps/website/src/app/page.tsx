39c1ec9777261239969134f701743bc43643f27d
Nobu <chat@nobu.sh>
2023-03-13 00:21:04 -0500

    chore: docs

---
+++ apps/website/src/app/page.tsx
@@ -0,0 +1,91 @@
+import { Inter } from 'next/font/google'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            By{' '}
+            <Image
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              height={24}
+              priority
+              src="/vercel.svg"
+              width={100}
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          alt="Next.js Logo"
+          className={styles.logo}
+          height={37}
+          priority
+          src="/next.svg"
+          width={180}
+        />
+        <div className={styles.thirteen}>
+          <Image alt="13" height={31} priority src="/thirteen.svg" width={40} />
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          className={styles.card}
+          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <h2 className={inter.className}>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          className={styles.card}
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <h2 className={inter.className}>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          className={styles.card}
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <h2 className={inter.className}>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
